3fe79446b6c32bc830c3250ad9bd2afddd696768
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-25 11:14:23 +0530

    Add topicsSlice

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const topicsSlice = createSlice({
+  name: "topics",
+  initialState: {
+    topics: {},
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      state.topics[id] = { id, name, icon, quizIds: [] };
+    },
+  },
+});
+
+export const selectTopics = (state) => state.topics.topics;
+export default topicsSlice.reducer;
