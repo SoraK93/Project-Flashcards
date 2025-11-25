226e21a3692abecc38b9deb38f516162935765e6
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-25 19:27:36 +0530

    Update: addQuizId reducer to topicsSlice

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -10,9 +10,13 @@ const topicsSlice = createSlice({
       const { id, name, icon } = action.payload;
       state.topics[id] = { id, name, icon, quizIds: [] };
     },
+    addQuizId: (state, action) => {
+      const { id, name, topicId, cardIds } = action.payload;
+      state.topics[topicId].quizIds.push(id);
+    },
   },
 });
 
 export const selectTopics = (state) => state.topics.topics;
-export const { addTopic } = topicsSlice.actions
+export const { addTopic, addQuizId } = topicsSlice.actions;
 export default topicsSlice.reducer;
